b8e881e841a985478c010c791ca1fe2e6860dcc0
ravalarupakumari-ui <ravalarupakumari@gmail.com>
2026-05-26 23:31:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Removed the strict extension to let the compiler find it automatically
+import App from './App.jsx' // Forced explicit path match
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
